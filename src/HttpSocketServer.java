96f1599ff57925ab2f50d181632eec0047b704b1
Catherine Korres <cathy.korres@gmail.com>
2021-12-06 22:32:10 -0300

    add client connection repetition

--- src/HttpSocketServer.java
+++ src/HttpSocketServer.java
@@ -1,2 +1,18 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class HttpSocketServer {
+    public static void main(String args[] ) throws IOException {
+        // create server socket
+        int port = 8081;
+        ServerSocket serverSocket = new ServerSocket(port);
+        System.out.println("✨️Listening to port " + port +"...");
+
+        // awaiting client connection
+         while(true) {
+             Socket clientSocket = serverSocket.accept();
+             System.out.println("[ New client connected ]");
+         }
+    }
 }
